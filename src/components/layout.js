ddc59bf54d42ee58df0c1c3b697f73f2d93fffb0
sixfwa <farhanaliw6@gmail.com>
2020-05-14 00:34:26 +0100

    favicon added

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+import favicon from "../images/favicon.ico"
 import Header from "./header"
 import Footer from "./footer"
 import "../styles/index.scss"
@@ -6,13 +8,19 @@ import layoutStyles from "./styles/layout.module.scss"
 
 const Layout = ({ children }) => {
   return (
-    <div className={layoutStyles.container}>
-      <div className={layoutStyles.content}>
-        <Header />
-        {children}
+    <React.Fragment>
+      <Helmet>
+        <link rel="icon" href={favicon} />
+      </Helmet>
+
+      <div className={layoutStyles.container}>
+        <div className={layoutStyles.content}>
+          <Header />
+          {children}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </React.Fragment>
   )
 }
 
