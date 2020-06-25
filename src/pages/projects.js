f5fcf19b9b83e979687ac22762870cf70c94a664
sixfwa <farhanaliw6@gmail.com>
2020-06-25 22:33:46 +0100

    Added DRF project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,6 +7,15 @@ import Project from "../components/items/project"
 const ProjectPage = () => {
   const [miniProjects] = useState([
     {
+      title: "Django REST Framework",
+      description:
+        "A REST API to store recipes. Built using DRF, Travis-CI, Docker and TDD.",
+      image:
+        "https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+      link: "https://github.com/sixfwa/recipe-app-api",
+      github: "https://github.com/sixfwa/recipe-app-api",
+    },
+    {
       title: "Quiz Web App",
       description:
         "Built using ReactJS. Built to help me better understand the Context API",
